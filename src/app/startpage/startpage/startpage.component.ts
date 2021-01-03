0e8c216b9b2d711928f5281533dbc606def47480
David <github@davidmannes.de>
2021-01-03 17:59:58 +0100

    Show username in welcomepage

--- src/app/startpage/startpage/startpage.component.ts
+++ src/app/startpage/startpage/startpage.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-startpage',
   templateUrl: './startpage.component.html'
 })
 export class StartpageComponent implements OnInit {
+  user: User
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) {
+    this.authService.user.subscribe(u => this.user = u);
+  }
 
   ngOnInit() {
   }
